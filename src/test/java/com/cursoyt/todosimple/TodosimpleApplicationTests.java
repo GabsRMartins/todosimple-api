45b9924e4f63cbfab299f45ca783fab452820e28
Gabs <gabsmartins262@gmail.com>
2024-09-27 08:09:44 -0300

    first commit

---
+++ src/test/java/com/cursoyt/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoyt.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
